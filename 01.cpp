81ae5df56818fda7a4d9e26f711a065899b6cb96
hwangjiyeong916 <bomaye119@gmail.com>
2024-07-21 05:18:45 +0900

    5th commit

--- 01.cpp
+++ 01.cpp
@@ -3,19 +3,38 @@
 using namespace std;
 
 int main(){
-  int n,m,i,j,a[101][101];
-  cin>>n>>m;
-  for(i=0; i<n; i++){
-    for(j=0; j<m; j++){
-      cin>>a[i][j];
+  long long int n,a[100]={0,10,20,30,40,50,60,70,80,90,1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000};
+  int i,h[10]={0},j=0;
+  string c[10],b[10]={"black","brown","red","orange","yellow","green","blue","violet","grey","white"} ;
+
+  for(i=0; i<3; i++)
+    cin>>c[i];
+  
+  for(i=0; i<10; i++){
+    if(c[j]==b[i]){
+      n=i*10;
+      j++;
+      break;
     }
-  }
-  for(i=0; i<n; i++){
-    for(j=0; j<m; j++){
-      cout<<a[i][j]<<" ";
-    }
-    cout<<"\n";
   }
 
+  for(i=0; i<10; i++){
+    if(c[j]==b[i]){
+      n+=i;
+      j++;
+      break;
+    }
+  }
+  
+  for(i=0; i<10; i++){
+    if(c[j]==b[i]){
+      n=n*a[10+i];
+      j++;
+      break;
+    }
+  }
+  
+  cout<<n;
+  
   return 0;
 }
