94ef2399673eb6d0659bf128d1829bf131a350f3
hwangjiyeong916 <bomaye119@gmail.com>
2024-07-21 02:04:12 +0900

    5th commit

--- 01.cpp
+++ 01.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
-#include <algorithm>
-#include<cmath>
+#include<iostream>
 
 using namespace std;
 
-int main()
-{
-   cout<<"Hello World!";
-   cout<<"hihicommitgi";
+int main(){
+  int n,m,i,j,a[101][101];
+  cin>>n>>m;
+  for(i=0; i<n; i++){
+    for(j=0; j<m; j++){
+      cin>>a[i][j];
+    }
+  }
+  for(i=0; i<n; i++){
+    for(j=0; j<m; j++){
+      cout<<a[i][j]<<" ";
+    }
+    cout<<"\n";
+  }
+
+  return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,11 +1,21 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-int main()
-{
-  unsigned long long a,b;
-  cin >> a >> b;
-  cout <<a+b;
+int main(){
+  int n,m,i,j,a[101][101];
+  cin>>n>>m;
+  for(i=0; i<n; i++){
+    for(j=0; j<m; j++){
+      cin>>a[i][j];
+    }
+  }
+  for(i=0; i<n; i++){
+    for(j=0; j<m; j++){
+      cout<<a[i][j];
+    }
+    cout<<"\n";
+  }
+
   return 0;
 }
