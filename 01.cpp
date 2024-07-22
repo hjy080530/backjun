a21d4189db25c2ef01ae0e48526c497f41fa1c94
hwangjiyeong916 <bomaye119@gmail.com>
2024-07-23 03:13:05 +0900

    7th commit

--- 01.cpp
+++ 01.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int n;
-  cin>>n;
-  for(int i=0; i<5; i++)
-    cout<<"hello"<<endl;
+  string a,c[10]={"NLCS","BHA","KIS","SJA"},b[10]={"North London Collegiate School","Branksome Hall Asia","Korea International School","t. Johnsbury Academy"};
+  int i;
+
+  cin>>a;
+
+  for(i=0; i<4; i++){
+    if(a==c[i]){
+      cout<<b[i];
+      break;
+    }
+      
+  }
+
   return 0;
 }
