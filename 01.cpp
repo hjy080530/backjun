67b7f189b03da5828baf554a9431d873e9bf5989
hwangjiyeong916 <bomaye119@gmail.com>
2024-07-24 01:00:04 +0900

    "9th commit"

--- 01.cpp
+++ 01.cpp
@@ -1,18 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-  string a,c[10]={"NLCS","BHA","KIS","SJA"},b[10]={"North London Collegiate School","Branksome Hall Asia","Korea International School","St. Johnsbury Academy"};
-  int i;
-
-  cin>>a;
-
-  for(i=0; i<4; i++){
-    if(a==c[i]){
-      cout<<b[i];
-      break;
-    }
-      
-  }
-
+  char a[100]="WelcomeToSMUPC";
+  int b;
+  cin>>b;
+  int x=b%14;
+  if(x==0)x=14;
+  cout<<a[x-1];
   return 0;
+   
 }
