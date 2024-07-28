315ce095225df9a22c7cb6340edc72e0973d40de
hwangjiyeong916 <bomaye119@gmail.com>
2024-07-28 18:33:53 +0900

    13th commit

--- 01.cpp
+++ 01.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-  int a,i;
-  cin>>a;
-  int b,sum=0;
-  for(i=0; i<a; i++){
-    cin>>b;
-    sum+=b;
-  }
-  cout<<sum;
+  cin<<dcnsl;
   return 0;
 }
-//걍 좀 해줘
