5dd4b3e02424599ca676f79a31447887a0a39042
hwangjiyeong916 <bomaye119@gmail.com>
2024-07-30 22:07:29 +0900

    15th commit

--- 01.cpp
+++ 01.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
-  cin<<dcnsl;
+  string a;
+  int i,j,b[1000]={0},mx=-9999999,ch=0;
+
+  cin>>a;
+  //snclksncl
+  for(i=0; i<a.size(); i++)
+    a[i]=toupper(a[i]);
+
+  for(i=0; i<a.size(); i++){
+    b[(int)a[i]]++;
+    mx=max(mx,b[i]);
+  }
+    
+  for(i=65; i<91; i++){
+    if(b[i]==b[mx]){
+      ch++;
+      break;
+    }
+  }
+
+  if(ch>1)
+    cout<<"?";
+  else
+    cout<<mx;
+
   return 0;
-  ///ncnsjkcd
 }
