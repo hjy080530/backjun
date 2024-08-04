71f08fd174395a5bb11a72de6974164903b7d247
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-04 22:54:03 +0900

    22th commit

--- 01.cpp
+++ 01.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-  int i,t,n,f,hap=0;
-
-  cin>>t>>n;
-
-  for(i=0; i<n; i++){
-    cin>>f;
-    hap+=f;
+  long long int i,n,hap=0;
+  cin>>n;
+  for(i=1; i<=n; i++){
+    if(n%i==0)hap+=i;
   }
-  if(hap>=t)cout<<"Padaeng_i Happy";
-  else cout<<"Padaeng_i Cry";
+  cout<<hap;
   return 0;
 }
