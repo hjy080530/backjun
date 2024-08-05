083683d8920d8c1c1304d1be08ebeedf1f7e5725
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-06 02:04:50 +0900

    25th commit

--- 01.cpp
+++ 01.cpp
@@ -3,18 +3,24 @@
 using namespace std;
 
 int main() {
-  string a;
-  long long int i,hap=0,ch[1001]={0},n,max[2]={0,0};
-
-  for(i=0; i<10; i++){
-    cin>>n;
-    hap+=n;
-    ch[n]++;
-    if(ch[n]>max[0]){
-      max[0]=ch[n];
-      max[1]=n;
+  int N,m,M,T,R,i,time=0;
+  cin>>N>>m>>M>>T>>R;
+  int H=m;
+  if(M-m<T){
+    cout<<-1;
+    return 0;
+  }
+  while(N){
+    if(H+T>M){
+      time++;
+      H-=R;
+    }
+    else{
+      H+=T;
+      time++;
+      N--;
     }
   }
-  cout<<hap/10<<"\n"<<max[1];
+  cout<<time;
   return 0;
 }
