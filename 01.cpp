6f11e03af8b97c2ee3845809cb35054696cd5dca
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-06 22:27:39 +0900

    26th commit

--- 01.cpp
+++ 01.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
-
+#include <stdio.h>
+#include <algorithm>
 using namespace std;
 
 int main() {
-  int N,m,M,T,R,i,time=0;
-  cin>>N>>m>>M>>T>>R;
-  int H=m;
-  if(M-m<T){
-    cout<<-1;
-    return 0;
-  }
-  while(N){
-    if(H+T>M){
-      time++;
-      H-=R;
+  int n,i,a,h=0;
+  cin>>n;
+  while(n){
+    cin>>a;
+    int k=0;
+    if(a==1){
+      n--;
+      continue;
     }
     else{
-      H+=T;
-      time++;
-      N--;
+      for(i=2; i<a; i++){
+        if(i%a==0){
+          k++; 
+          break;
+        }
+      }
+      if(k==0){
+        h++;
+      }
     }
+    n--;
   }
-  cout<<time;
+  cout<<h;
+
   return 0;
 }
