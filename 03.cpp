99f1d20217468d54c99a7af4d572c9a47ff5e4d7
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-08 22:23:20 +0900

    29th commit

--- 03.cpp
+++ 03.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-int main()
-{
-    int n,i,j; 
-    cin>>n;
-    int da[n+1],su[n+1],h[n+1];
 
-    for(i=0; i<n; i++){
-        cin>>da[i];
-        h[i]=da[i];
+int main(){
+    string a;
+    int i,h=0;
+
+    getline(cin,a);
+
+    for(i=0; i<a.size(); i++){
+        if(a[i]==' ')h++;
     }
-    sort(da,da+n);
-    for(i=0; i<n; i++)su[i]=i+da[i];
-    for(i=0; i<n; i++){
-        for(j=0; j<n; i++){
-            if(da[i]==h[j]){
-            cout<<su[i]-da[i]<<" ";
-            }
-        }
-        
-    }
-    
+
+    cout<<h;
     return 0;
 }
