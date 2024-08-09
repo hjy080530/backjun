4375161c45e790c740beab6f4ad59f39e5a840ef
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-10 04:35:30 +0900

    32th commit

--- 01.cpp
+++ 01.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
 int main() {
-  long long int N,a,b;
+  long long int N,a,b,i;
+  long long int h = 0;
+    
   cin>>N;
-  bool ga[N+1]={false},se[N+1]={false};
-  while(N--;){
+
+  map<long long int, int> ga;
+  map<long long int, int> se;
+
+  for(i=0;i<N;i++) {
     cin>>a>>b;
-
+    ga[a]++;
+    se[b]++;
   }
+  for (auto &x:ga) {
+    if (x.second > 1) h++;
+  }
+  for (auto&y:se) {
+    if (y.second > 1) h++; 
+  }
+  cout<<h<<endl;
 
-  return 0;
+    return 0;
 }
