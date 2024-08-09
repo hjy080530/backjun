ae29dbba827dec15147684c6217b76a688625154
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-09 21:29:54 +0900

    30th commit

--- 03.cpp
+++ 03.cpp
@@ -6,7 +6,9 @@ int main(){
     int i,h=0;
 
     getline(cin,a);
-
+    if(a[0]==' '||a[a.size()]==' ') h=0;
+    else if(a[0]==' '&&a[a.size()]==' ')h=-1;
+    else h++;
     for(i=0; i<a.size(); i++){
         if(a[i]==' ')h++;
     }
