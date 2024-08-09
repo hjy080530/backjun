2398bf73746d0459d32596630e0d85aacd6e1f22
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-10 00:55:26 +0900

    31th commit

--- 01.cpp
+++ 01.cpp
@@ -2,25 +2,13 @@
 using namespace std;
 
 int main() {
-  int n,m,i,ac[1000],h=0,j;
-  string a[1000],b;
-  cin>>n;
-  for(i=0; i<n; i++)cin>>a[i];
-  cin>>m;
-  for(i=0; i<m; i++){
-    cin>>b;
-    for(j=0; j<n; j++){
-      if(b==a[j]){
-        h++;
-        ac[j]=1;
-      }
-    }
+  long long int N,a,b;
+  cin>>N;
+  bool ga[N+1]={false},se[N+1]={false};
+  while(N--;){
+    cin>>a>>b;
+
   }
-  cout<<h<<"\n";
-  for(i=0; i<n; i++){
-    if(ac[i]==0){
-      cout<<a[i]<<"\n";
-    }
-  }
+
   return 0;
 }
