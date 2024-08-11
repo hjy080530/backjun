1ad50debb52fae5feee10a501b2ed8383d8e52d0
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-12 00:31:31 +0900

    33th commit

--- 01.cpp
+++ 01.cpp
@@ -1,28 +1,11 @@
 #include <iostream>
-#include <map>
 using namespace std;
 
 int main() {
-  long long int N,a,b,i;
-  long long int h = 0;
-    
-  cin>>N;
-
-  map<long long int, int> ga;
-  map<long long int, int> se;
-
-  for(i=0;i<N;i++) {
-    cin>>a>>b;
-    ga[a]++;
-    se[b]++;
-  }
-  for (auto &x:ga) {
-    if (x.second > 1) h++;
-  }
-  for (auto&y:se) {
-    if (y.second > 1) h++; 
-  }
-  cout<<h<<endl;
-
-    return 0;
-}
+  int a,b,c;
+  cin>>a>>b>>c;
+  if(c<100||b<10)cout<<a<<b<<0<<c;
+  else if(c>100||b<10)cout<<a<<b<<c;
+  \;
+  return 0;
+}...dcndwcs
