c29b5197cc3a3a2c4288784d788a1e370b7ca488
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-12 00:51:52 +0900

    33th commit

--- 01.cpp
+++ 01.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 
 int main() {
-  int a,b,c;
-  cin>>a>>b>>c;
-  if(c<100||b<10)cout<<a<<b<<0<<c;
-  else if(c>100||b<10)cout<<a<<b<<c;
-  \;
+  long long int i,T,a,j;
+  cin>>T;
+  for(i=0; i<T; i++){
+    cin>>a;
+    j=0;
+    while(a){
+      if(a%2==1)cout<<j<<" ";
+      a=a/2;
+      j++;
+    }
+  }
   return 0;
-}...dcndwcs
+}
