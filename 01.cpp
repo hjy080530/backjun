a82f0ce842b54d1156ffc56bc7e310c94f17672d
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-14 00:26:01 +0900

    34th commit

--- 01.cpp
+++ 01.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
-#include<stdio.h>
 using namespace std;
-
-int main() {
-  int a,b;
-
-  cin>>a>>b;
-  if(a%b==0)cout<<a/b;
-  else printf("%.10f",(double)a/b);
+int main()
+{
+  long long int a,arr[1000000],n,i;
+  cin>>n;
+  for(i=0; i<n; i++){
+    cin>>a;
+    arr[a]=1;
+  }
+  cin>>n;
+  for(i=0; i<n; i++){
+    cin>>a;
+    cout<<arr[a]<<"\n";
+  }
   return 0;
 }
