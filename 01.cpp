a627a49c5f6ee5160d63a092158e15d7c369af5e
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-16 22:21:34 +0900

    34th commit

--- 01.cpp
+++ 01.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main()
 {
-  long long int a,arr[1000000],n,i;
+  long long int n,i,p[10]={0};
   cin>>n;
-  n--;
-  cout<<(n/5)+1;
+/ㅊㅊ
+  while(n){
+    p[n%10]++;
+    n=n/10;
+  }
+  if(p[6]>p[9])p[6]=p[6]-p[9];
+  else p[9]=p[9]-p[6];
+  n=max_element(p,p+10);
+  cout<<n;
   return 0;
-}..c.
+}
