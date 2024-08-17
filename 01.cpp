a453584c366a78f512e070d9013b7f5ad908e97f
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-17 19:15:22 +0900

    36th commit

--- 01.cpp
+++ 01.cpp
@@ -1,34 +1,51 @@
 #include <iostream>
-#include <stack>
-#include <algorithm>
+#include <queue>
+#include <string>
 using namespace std;
-int main(){
-  stack <int> st;
-  int n,k;
-  string a;
 
-  cin>>n;
+int main() {
+    queue <int> st;
+    int n, k;
+    string a;
 
-  while(n){
-    cin>>a;
-    if(a=="push"){
-      cin>>k;
-      st.push(k);
+    cin >> n;
+
+    while (n--) {  // n번 반복하기 위해 --n 사용
+        cin >> a;
+
+        if (a == "push") {
+            cin >> k;
+            st.push(k);
+        } 
+        else if (a == "pop") {
+            if (st.empty()) {
+                cout << "-1\n";
+            } else {
+                cout << st.front() << "\n"; 
+                st.pop();
+            }
+        } 
+        else if (a == "size") {
+            cout << st.size() << "\n";
+        } 
+        else if (a == "empty") {
+            cout << (st.empty() ? 1 : 0) << "\n";
+        } 
+        else if (a == "front") {
+            if (st.empty()) {
+                cout << "-1\n";
+            } else {
+                cout << st.front() << "\n";
+            }
+        }
+        else if (a == "back") {
+            if (st.empty()) {
+                cout << "-1\n";
+            } else {
+                cout << st.back() << "\n";
+            }
+        }
     }
-    else if(a=="pop"){
-      if(st.empty())cout<<"-1\n";
-      else st.pop();
-    }
-    else if(a=="size")cout<<st.size()<<"\n";
-    else if(a=="empty"){
-      if(st.empty())cout<<"1\n";
-      else cout<<"0\n";
-    }
-    else if(a=="top"){
-      if(st.empty())cout<<"-1\n";
-      else cout<<st.top()<<"\n";
-    }
-    n--;
-  }
-  return 0;
+    
+    return 0;
 }
