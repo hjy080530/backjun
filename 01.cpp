9a7626a1649b13c432dc4c9eea2eb41cc696f943
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-18 20:29:45 +0900

    36th commit

--- 01.cpp
+++ 01.cpp
@@ -1,51 +1,13 @@
 #include <iostream>
-#include <queue>
-#include <string>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
 int main() {
-    queue <int> st;
-    int n, k;
-    string a;
+  int h,w;
 
-    cin >> n;
+  cin>>h>>w;
 
-    while (n--) {  // n번 반복하기 위해 --n 사용
-        cin >> a;
-
-        if (a == "push") {
-            cin >> k;
-            st.push(k);
-        } 
-        else if (a == "pop") {
-            if (st.empty()) {
-                cout << "-1\n";
-            } else {
-                cout << st.front() << "\n"; 
-                st.pop();
-            }
-        } 
-        else if (a == "size") {
-            cout << st.size() << "\n";
-        } 
-        else if (a == "empty") {
-            cout << (st.empty() ? 1 : 0) << "\n";
-        } 
-        else if (a == "front") {
-            if (st.empty()) {
-                cout << "-1\n";
-            } else {
-                cout << st.front() << "\n";
-            }
-        }
-        else if (a == "back") {
-            if (st.empty()) {
-                cout << "-1\n";
-            } else {
-                cout << st.back() << "\n";
-            }
-        }
-    }
-    
-    return 0;
+  
+  return 0;
 }
