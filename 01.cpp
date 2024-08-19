794cf5e8d12cb3811fbef742269d082812e7bb62
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-20 00:36:28 +0900

    39th commit

--- 01.cpp
+++ 01.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
-#include <algorithm>
-  using namespace std;
+
+using namespace std;
+
 int main()
 {
-  int i,n,p[1000];
-  cin>>n;
-  for(i=0; i<n; i++)cin>>p[i];
-  sort(p,p+n);
-  for(i=n-1; i>=0; i--)cout<<p[i]<<" ";
-	return 0;//ㅇㄴㅊ
+  long long int n,m,i,j;
+  cin>>n>>m;
+  for(i=n; i<m; i++){
+    int x=0;
+    for(j=2;j<i; j++){
+      if(i%j==0){
+        x++;
+        break;
+      }
+    }
+    if(x==0)cout<<i<<"\n";
+  }
+	return 0;
 }
