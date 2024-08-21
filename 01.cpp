78d9895123e81d43a7c48527a7493212b8c8f892
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-21 09:37:07 +0900

    41th commit

--- 01.cpp
+++ 01.cpp
@@ -1,19 +1,25 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
+#include <stdio.h>
 
-int main()
-{
-  int i,P,a[1000],N,count=0;
-  cin>>P>>N;
-  for(i=0; i<N; i++) cin>>a[i];
-  sort(a,a+N);
-  for(i=0; i<N; i++){
-    if(P<200){
-      P+=a[i];
-      count++;
+// 재귀적으로 2진수 변환을 수행하는 함수
+void decimalToBinary(int n) {
+    if (n == 0) {
+        return;
     }
-  }
-  cout<<count;
-	return 0;
+    decimalToBinary(n / 2);  // n을 2로 나눈 몫을 가지고 재귀 호출
+    printf("%d", n % 2);     // n을 2로 나눈 나머지를 출력 (2진수의 현재 자리)
+}
+
+int main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    //ㅁㅇ 
+    if (num == 0) {
+        printf("0");
+    } else {
+        decimalToBinary(num);
+    }
+
+    printf("\n");
+    return 0;
 }
