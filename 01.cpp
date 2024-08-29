3fd8867712c7a7888df40ccdf78785c2b6b31abc
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-29 15:38:53 +0900

    48th commit

--- 01.cpp
+++ 01.cpp
@@ -3,22 +3,14 @@
 using namespace std;
 
 int main() {
-    int a[10],i,j;
-    for(i=0; i<3; i++){
-        for(j=0; j<6; j++)cin>>a[j];
-         a[5]-=a[2];
-         if(a[5]>59){
-            a[5]-=60;
-            a[4]--;
-            if(a[5]>)
-        }
-         a[4]-=a[1];
-         if(a[4]>59){
-            a[1]-=100;
-            a[3]--;
-        }
-        a[3]-=a[0];
-        cout<<"\n"<<a[3]<<" "<<a[4]<<" "<<a[5];
+    int n,count=0;
+    string a,k;
+    cin>>k>>n;
+    while(n){
+        cin>>a;
+        if(a==k)count++;
+        n--;
     }
+    cout<<count;
     return 0;
 }
