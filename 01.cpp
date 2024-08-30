ab9abd582f0c6ebc70f60b121387a6eb921d8ff5
hwangjiyeong916 <bomaye119@gmail.com>
2024-08-30 23:39:21 +0900

    48th commit

--- 01.cpp
+++ 01.cpp
@@ -3,14 +3,16 @@
 using namespace std;
 
 int main() {
-    int n,count=0;
-    string a,k;
-    cin>>k>>n;
+    int a,i,n,c;
+    cin>>n;
     while(n){
-        cin>>a;
-        if(a==k)count++;
+        cin>>a>>c;
+        if(a>c)a=(a/c)+1;
+        if(a%c>0)a=(a/c)+1;
+        else a=a/c;
+        cout<<a;
         n--;
     }
-    cout<<count;
+
     return 0;
 }
