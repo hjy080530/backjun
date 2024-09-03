28b9a5088bac971e3b3fb279e5e1b1cddb62bc73
hwangjiyeong916 <bomaye119@gmail.com>
2024-09-03 22:36:16 +0900

    48th commit

--- 01.cpp
+++ 01.cpp
@@ -1,45 +1,11 @@
 #include <iostream>
-#include <stdio.h> 
-#include <vector>
+#include <stdio.h>
 using namespace std;
-void Hex(long long n) {
-    vector<int> digits;
-    
-    while (n > 0) {
-        digits.push_back(n % 16);
-        n /= 16;
-    }
 
-    for (int i = digits.size() - 1; i >= 0; i--) {
-        cout << digits[i];
-    }
-    cout<<"\n";
-}
-void Oct (long long n){
-    vector <int>digi ;
-    while (n > 0) {
-        digi.push_back(n % 8);
-        n /= 8;
-    }
-
-    for (int i = digi.size() - 1; i >= 0; i--) {
-        cout << digi[i];
-    }
-    cout<<" ";
-}
-int main() {
-    long long int h,i, t, k, n;
-    cin >> t; 
-
-    for (i = 0; i < t; i++) {
-        cin >> k >> n;
-        h=n; 
-        if(n<8)h=0;
-        printf("\n %lld ", k);
-        Oct(n);
-        cout<<n<<" ";
-        Hex(n);
-    }
-    
+int main(){
+    double r,x;
+    cin>>r;
+    x=r-1;
+    printf("%.2f",x);
     return 0;
 }
