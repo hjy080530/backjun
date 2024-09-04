9f7fb1c23a1a8485173f9cfd4e47d40e9c95e444
hwangjiyeong916 <bomaye119@gmail.com>
2024-09-04 09:01:21 +0900

    48th commit

--- 01.cpp
+++ 01.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
-#include <stdio.h>
+#include <string>
+#include <stdlib.h>
 using namespace std;
 
 int main(){
-    double r,x;
-    cin>>r;
-    x=r-1;
-    printf("%.2f",x);
+    string a,b,c;
+    cin>>a;
+    cin>>c;
+    cin>>b;
+
+    int lena=a.length(),lenb=b.length(),h;
+    if(c[0]=='*'){
+        printf("1");
+        for(int i=0; i<lena+lenb-2; i++){
+            printf("0");
+        }
+    }
+    else{
+        h=min(lena,lenb);
+        lena=max(lena,lenb);
+        printf("1");
+        for(int i=1; i<lena; i++){
+            if(i==(lena-h)){
+                printf("1");
+            }
+            else printf("0");
+        }
+    }
+
     return 0;
 }
