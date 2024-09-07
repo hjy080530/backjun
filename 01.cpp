c0c98842b09a8e5199f3fb0b9143ed1fa312594d
hwangjiyeong916 <bomaye119@gmail.com>
2024-09-07 13:12:05 +0900

    48th commit

--- 01.cpp
+++ 01.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
-#include <string>
-#include <stdlib.h>
+#include <algorithm>
 using namespace std;
 
-int main(){
-    string a,b,c;
-    cin>>a;
-    cin>>c;
-    cin>>b;
+int ten(int n) {
+    if (n < 10) return n;      
+    return ten(n / 10);
+}
+int main() {
+    int n, a[10001], b[10001];
+    cin >> n;
 
-    int lena=a.length(),lenb=b.length(),h;
-    if(c[0]=='*'){
-        printf("1");
-        for(int i=0; i<lena+lenb-2; i++){
-            printf("0");
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> a[j];
         }
+        sort(a, a + 5);  
+        int hap = 0;
+        for (int j = 0; j < 3; j++) {
+            hap += a[j]; 
+        }
+        b[i] = ten(hap); 
     }
-    else{
-        h=min(lena,lenb);
-        lena=max(lena,lenb);
-        printf("1");
-        for(int i=1; i<lena; i++){
-            if(i==(lena-h)){
-                printf("1");
-            }
-            else printf("0");
+
+    int *c = max_element(b + 1, b + n + 1); 
+    for (int i = 1; i <= n; i++) {
+        if (*c == b[i]) { 
+            cout << i << endl;
+            return 0;
         }
     }
 
